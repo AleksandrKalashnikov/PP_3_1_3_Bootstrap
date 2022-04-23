87082e9190a19596ba18d8e4bf9c86df2154cddb
drkpo <drkpolice666@@gmail.com>
2022-04-23 12:02:10 +0300

    23 Вроде работает

--- src/main/java/com/example/pp_3_1_3_bootstrap/configs/WebSecurityConfig.java
+++ src/main/java/com/example/pp_3_1_3_bootstrap/configs/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -37,7 +36,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .successHandler(successUserHandler)
                 .and()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
                 .logoutSuccessUrl("/");
     }
 
